332bb78e15bd273e3552d470c2fcb7eecd994250
LorenzoA93 <lorenzoestebanaraya@gmail.com>
2023-02-11 22:32:27 -0300

    11/02/2023

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import landing from './components/landing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+    <landing/>
+
+    
   </React.StrictMode>
 );
 
@@ -15,3 +16,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//no pude importar bien el landing
