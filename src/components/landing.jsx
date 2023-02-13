73fcbac7414f968649bacd8ee5f3e1e2ed243950
LorenzoA93 <lorenzoestebanaraya@gmail.com>
2023-02-12 21:44:23 -0300

    12/02/2023

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import ItemListContainer from './ItemListContainer/ItemListContainer'
 import NavBar from './NavBar/NavBar'
 
-const landing = () => {
+const Landing = () => {
   return (
+    <>
     <NavBar/>
+    <ItemListContainer greeting="Bienvenido a nuestra tienda virtual"/>
+    </>
   )
 }
 
-export default landing
+export default Landing
