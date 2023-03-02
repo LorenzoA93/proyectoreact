cb835a1bbaf28c615dde212c2b7a18ff43d60c54
LorenzoA93 <lorenzoestebanaraya@gmail.com>
2023-03-02 13:55:50 -0300

    02/03/2023

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import ItemList from './ItemList';
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
+const ItemListContainer = (prop) => {
 
-const ItemListContainer = () => {
-  return (
-    <section className="item-list-container">
-      <h2 className="item-list-container__title">Tipos de Matafuegos</h2>
+    const { categoryId } = useParams()
+    const [itemList, setItemList] = useState([])
 
-      <ItemList />
-    </section>
-  );
-};
+    useEffect(() => {
+        fetch(``)
+            .then(response => response.json())
+            .then(dataJson => setItemList(dataJson))
+    }, [categoryId])
 
+    return (
+        <>
+            {
+                prop.greeting !== undefined ?
+                    <h2 className='greeting'>{prop.greeting}</h2>
+                    :
+                    <ul className='items'>
+                        {
+                            itemList?.map(item =>
+                                <li key={item.id}>
+                                    <img src={item.image} alt="" />
+                                    <h3>{item.title}</h3>
+                                    <h4>${item.price}</h4>
+                                    <Link to={`/item/${item.id}`}>VER DETALLES</Link>
+                                </li>
+                            )
+                        }
+                    </ul>
+            }
+        </>
+    )
+}
 export default ItemListContainer
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import IndumentariaView from './components/Indumentaria/IndumentariaView';
+import IndumentariaView from './components/Pages/Indumentaria/IndumentariaView';
 import Landing from './components/Landing';
-import MatafuegosView from './components/Matafuegos/MatafuegosView';
+import MatafuegosView from './components/Pages/Matafuegos/MatafuegosView';
 import NavBar from './components/NavBar/NavBar';
-import ProtectoresCorporalesView from './components/ProtectoresCorporales/ProtectoresCorporalesView';
+import ProtectoresCorporalesView from './components/Pages/ProtectoresCorporales/ProtectoresCorporalesView';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,9 +17,9 @@ root.render(
       <Routes>
 
         <Route exact path='/' element= {<Landing/>}/>
-        <Route exact path='/Matafuegos' element= {<MatafuegosView/>}/>
-        <Route exact path='/Indumentaria' element={<IndumentariaView/>}/>
-        <Route exact path='/ProtectoresCorporales' element={<ProtectoresCorporalesView/>}/>
+        <Route exact path='/Pages/Matafuegos' element= {<MatafuegosView/>}/>
+        <Route exact path='/Pages/Indumentaria' element={<IndumentariaView/>}/>
+        <Route exact path='/Pages/ProtectoresCorporales' element={<ProtectoresCorporalesView/>}/>
 
 
       </Routes>
